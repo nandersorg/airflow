13050660d20b520e2511ac39a4ee66a06d05de55
Nander Speerstra <6020091+nanderspeerstra@users.noreply.github.com>
2026-07-08 19:58:29 +0000

    fix(dags): import error fix for quickdraw

--- dags/quickdraw_retrain_dag.py
+++ dags/quickdraw_retrain_dag.py
@@ -1,6 +1,6 @@
 import datetime
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+from airflow.providers.cncf.kubernetes.operators.pod import (
     KubernetesPodOperator,
 )
 from kubernetes.client import models as k8s
